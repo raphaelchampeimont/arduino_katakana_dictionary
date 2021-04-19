7664502402d3029a7c3661abf550090c691a0902
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-04-19 12:56:10 +0200

    clean-up code

--- katakana/katakana.ino
+++ katakana/katakana.ino
@@ -4,8 +4,6 @@
 const int SELECTOR_PIN = A0;
 
 // Connections to the circuit: LCD screen
-const int LCD_ROWS = 2;
-const int LCD_COLS = 16;
 const int LCD_RS_PIN = 12;
 const int LCD_ENABLE_PIN = 11;
 const int LCD_DATA_PIN_0 = 5;
@@ -15,6 +13,8 @@ const int LCD_DATA_PIN_3 = 2;
 
 LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_DATA_PIN_0, LCD_DATA_PIN_1, LCD_DATA_PIN_2, LCD_DATA_PIN_3);
 
+const int LCD_ROWS = 2;
+const int LCD_COLS = 16;
 const int ARRAY_SIZE = 56;
 const int KANAS_PER_PAGE = 4;
 const int CHARS_PER_KANA = 4;
@@ -39,8 +39,6 @@ const char* translations[ARRAY_SIZE] = {
 int lastSelectedPage = -1;
 
 void setup() {
-  // put your setup code here, to run once:
-
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
@@ -50,17 +48,17 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int selectorRawValue = analogRead(SELECTOR_PIN);
   int selectedPage = map(selectorRawValue, 0, 1023, 0, N_PAGES-1);
   if (lastSelectedPage != selectedPage) {
     lastSelectedPage = selectedPage;
 
-    // for debug
+    // Switch on internal LED for debug
     digitalWrite(LED_BUILTIN, HIGH);
     
     lcd.clear();
 
+    // Display katakanas and their translation
     for (int i=0; i<KANAS_PER_PAGE; i++) {
       int index = selectedPage*KANAS_PER_PAGE + i;
       lcd.setCursor(i*CHARS_PER_KANA, 0);
@@ -69,12 +67,12 @@ void loop() {
       lcd.write(0xA6 + index);
     }
 
-    // debug
+    // Display page number
     int pageNumber = selectedPage + 1;
     lcd.setCursor(LCD_COLS - (pageNumber < 10 ? 1 : 2), 1);
     lcd.print(pageNumber, DEC);
 
-    // for debug
+    // Switch off internal LED for debug
     digitalWrite(LED_BUILTIN, LOW);
   }
 }
