d39663bf6bf60f4a34d8ce4b092ec0bb7e26c8b9
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-04-28 10:20:35 +0200

    Fix incorrect pin naming
    Pin naming did not follow convention of LCD screen.

--- katakana/katakana.ino
+++ katakana/katakana.ino
@@ -6,12 +6,12 @@ const int SELECTOR_PIN = A0;
 // Connections to the circuit: LCD screen
 const int LCD_RS_PIN = 12;
 const int LCD_ENABLE_PIN = 11;
-const int LCD_DATA_PIN_0 = 5;
-const int LCD_DATA_PIN_1 = 4;
-const int LCD_DATA_PIN_2 = 3;
-const int LCD_DATA_PIN_3 = 2;
+const int LCD_DATA_PIN_4 = 5;
+const int LCD_DATA_PIN_5 = 4;
+const int LCD_DATA_PIN_6 = 3;
+const int LCD_DATA_PIN_7 = 2;
 
-LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_DATA_PIN_0, LCD_DATA_PIN_1, LCD_DATA_PIN_2, LCD_DATA_PIN_3);
+LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_DATA_PIN_4, LCD_DATA_PIN_5, LCD_DATA_PIN_6, LCD_DATA_PIN_7);
 
 const int LCD_ROWS = 2;
 const int LCD_COLS = 16;
